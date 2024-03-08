51d9e8744c9ae0cae3bae6b3874bbad5f3505efd
adetech2017 <adeyemi879@gmail.com>
2024-03-08 09:41:45 +0100

    form modification update

--- src/components/CustomModal.js
+++ src/components/CustomModal.js
@@ -16,6 +16,27 @@ const CustomModal = ({ showModal, handleClose }) => {
     const handleFormSubmit = async (e) => {
         e.preventDefault();
 
+        // Simple form validation
+        const formElements = e.target.elements;
+        let hasErrors = false;
+
+        for (let i = 0; i < formElements.length; i++) {
+            const element = formElements[i];
+
+            if (element.required && element.value.trim() === "") {
+                setValidationErrors((prevErrors) => ({
+                    ...prevErrors,
+                    [element.name]: "This field is required.",
+                }));
+                hasErrors = true;
+            }
+        }
+
+        if (hasErrors) {
+            // Stop form submission if there are validation errors
+            return;
+        }
+
         setLoading(true);
 
         // Wait for the form submission to complete
@@ -45,14 +66,14 @@ const CustomModal = ({ showModal, handleClose }) => {
         <Modal show={showModal} onHide={handleClose} className="custom-modal">
             <Modal.Header closeButton>
                 <Modal.Title className="text-center">
-                Receive Your Free Cash Offer TODAY!
+                    Receive Your Free Cash Offer TODAY!
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
                 <Form onSubmit={handleFormSubmit}>
                     <Form.Group controlId="formHowQuickly">
                         <Form.Label>How quickly are you looking to sell? *</Form.Label>
-                            <Form.Select name="how_quick" id="how_quick" aria-label="How quickly are you looking to sell?">
+                            <Form.Select name="HowQuick" id="HowQuick" aria-label="How quickly are you looking to sell?">
                             <option>Select option</option>
                             <option value="1">1 to 2 Months</option>
                             <option value="2">2 to 4 Months</option>
@@ -63,27 +84,27 @@ const CustomModal = ({ showModal, handleClose }) => {
 
                     <Form.Group className="mb-3" controlId="forSelling">
                         <Form.Label>What is the reason for selling? *</Form.Label>
-                        <Form.Control type="text" name="reason" id="reason" placeholder="Reason for selling" />
+                        <Form.Control type="text" name="Reason" id="Reason" placeholder="Reason for selling" />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="forEstimated">
                         <Form.Label>Estimated value of your property *</Form.Label>
-                        <Form.Control type="number" name="estimated_value" id="estimated_value" placeholder="Estimated value" />
+                        <Form.Control type="number" name="EstimatedValue" id="EstimatedValue" placeholder="Estimated value" />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="forMortage">
                         <Form.Label>What is your outstanding mortgage? *</Form.Label>
-                        <Form.Control type="number" name="outstanding_mortgage" id="outstanding_mortgage" placeholder="If no mortgage, please add 0" />
+                        <Form.Control type="number" name="OutstandingMortgage" id="OutstandingMortgage" placeholder="If no mortgage, please add 0" />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="forProperty">
                         <Form.Label>Do you have a secured loan/s on this property? *</Form.Label>
-                        <Form.Control type="number" name="existing_loan" id="existing_loan" placeholder="If so, add amount. If not, add 0" />
+                        <Form.Control type="number" name="ExistingLoan" id="ExistingLoan" placeholder="If so, add amount. If not, add 0" />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="forOblibation">
                         <Form.Label>Are you happy for us to contact you for a no obligation informal chat? *</Form.Label>
-                        <Form.Select name="obligation" id="obligation" aria-label="Default select example">
+                        <Form.Select name="Obligation" id="Obligation" aria-label="Default select example">
                             <option>Select option</option>  
                             <option value="no">No</option>
                             <option value="yes">Yes</option>
@@ -92,36 +113,62 @@ const CustomModal = ({ showModal, handleClose }) => {
 
                     <Form.Group className="mb-3" controlId="forName">
                         <Form.Label>Full Name *</Form.Label>
-                        <Form.Control name="name" id="name" type="text" placeholder="Fullname" />
+                        <Form.Control name="FullName" id="FullName" type="text" placeholder="Fullname" />
+                        <ValidationError prefix="FullName" field="FullName" errors={state.errors}/>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="forPhone">
                         <Form.Label>Phone *</Form.Label>
-                        <Form.Control name="phone" id="phone" type="text" placeholder="Phone number" />
+                        <Form.Control name="Phone" id="Phone" type="text" placeholder="Phone number" />
+                        <ValidationError prefix="Phone" field="Phone" errors={state.errors}/>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="forEmail">
                         <Form.Label>Email *</Form.Label>
-                        <Form.Control name="email" id="email" type="email" placeholder="Email" />
+                        <Form.Control name="Email" id="Email" type="email" placeholder="Email" />
+                        <ValidationError prefix="Email" field="email" errors={state.errors}/>
                     </Form.Group>
 
                     <Form.Group className="mb-3">
                         <Form.Label>Address of property *</Form.Label>
-                        <Form.Control name="address" id="address" type="text" placeholder="Address" />
+                        <Form.Control name="Address" id="Address" type="text" placeholder="Address" />
+                        <ValidationError prefix="Address" field="Address" errors={state.errors}/>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formCity">
-                    <Form.Label>City *</Form.Label>
-                    <Form.Control name="city" id="city" type="city" placeholder="City" />
+                        <Form.Label>City *</Form.Label>
+                        <Form.Control name="City" id="City" type="city" placeholder="City" />
+                        <ValidationError prefix="City" field="City" errors={state.errors}/>
                     </Form.Group>
                     
                     <Form.Group className="mb-3" controlId="formPostalCode">
                         <Form.Label>Postal code *</Form.Label>
-                        <Form.Control name="postcode" id="postcode" type="text" placeholder="Postal code" />
+                        <Form.Control name="PostCode" id="PostCode" type="text" placeholder="Postal code" />
+                        <ValidationError prefix="PostCode" field="PostCode" errors={state.errors}/>
                     </Form.Group>
 
+                    <Form.Group className="mb-3" controlId="formTermsAndConditions">
+                        <Form.Check
+                            type="checkbox"
+                            label="I agree to the Terms and Conditions"
+                            name="TermsAndConditions"
+                            id="termsAndConditions"
+                            required
+                        />
+                        <Form.Text className="text-muted">
+                            <a href="/terms-and-conditions" target="_blank" rel="noopener noreferrer">Terms and Conditions</a>.
+                        </Form.Text>
+                    </Form.Group>
+
+                    {/* Display validation errors */}
+                    {Object.keys(validationErrors).map((fieldName) => (
+                        <p key={fieldName} className="text-danger">
+                            {validationErrors[fieldName]}
+                        </p>
+                    ))}
+
                     <div className="d-grid gap-2">
-                        <Button type="submit" disabled={loading} variant="secondary" className="custom-submit-button" size="lg">
+                        <Button type="submit" disabled={state.submitting} variant="secondary" className="custom-submit-button" size="lg">
                             {loading ? (
                                 <>
                                     <Spinner animation="border" size="sm" className="me-2" />
